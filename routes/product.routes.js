b065c9e54c0f10f023bb0930beb57f0b3ab2fe2d
aldaine-AG <aldaineclarke1@gmail.com>
2022-10-07 08:12:09 -0500

    renamed products to product

---
+++ routes/product.routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const ProductController = require('../controllers/products.controller');
+const { uploadFileToS3 } = require('../utilities/s3');
+
+router
+    .route("/")
+    .get(ProductController.getAllProducts)
+    .post(uploadFileToS3.single("prod_img") ,ProductController.createProduct);
+
+router
+    .route("/:id")
+    .get(ProductController.getProductById)
+    .patch(uploadFileToS3.single("prod_img"),ProductController.updateProduct)
+    .delete(ProductController.deleteProduct)
+
+
+module.exports = router;
