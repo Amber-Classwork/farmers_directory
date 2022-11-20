ed7054982f5654298672bca605bd2efa627503e1
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-20 13:46:26 -0500

    updated the index file to show version 2 of the api

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 8000;
-const APIVERSION1 = require("./api/api-v1");
+const APIVERSION1 = require("./api/v1/api-v1");
+const APIVERSION2 = require("./api/v2/api-v2");
 const mongoose = require("mongoose");
 // const { swaggerDocs: V1SwaggerDocs} = require( "./swagger")
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api/v1", APIVERSION1);
+app.use("/api/v2", APIVERSION2);
 app.get("/", (req, res, next)=>{
    res.status(200).sendFile(__dirname+"/index.html");
 })
