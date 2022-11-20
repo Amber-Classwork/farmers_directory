91bbd9d075600b73a083c9c1f2147908dad67d8f
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-20 13:47:40 -0500

    updated folder structure to have version 1 of the api in a separate folder

---
+++ api/v1/api-v1.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+router.use("/farmers", require("../../routes/farmer.routes"));
+router.use("/categories", require("../../routes/category.routes"));
+router.use("/products", require("../../routes/product.routes"));
+router.use("/users", require("../../routes/user.routes"));
+
+
+
+module.exports = router;
