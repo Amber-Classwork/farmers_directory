f7039824dfff5570b167f920be9c55e25dcc3d91
aldaine-AG <aldaineclarke1@gmail.com>
2022-11-20 15:05:11 -0500

    updated path for api v1 to fix loader error

--- api/v1/api-v1.js
+++ api/v1/api-v1.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-router.use("/farmers", require("../../routes/farmer.routes"));
-router.use("/categories", require("../../routes/category.routes"));
-router.use("/products", require("../../routes/product.routes"));
-router.use("/users", require("../../routes/user.routes"));
+router.use("/farmers", require("./routes/farmer.routes"));
+router.use("/categories", require("./routes/category.routes"));
+router.use("/products", require("./routes/product.routes"));
+router.use("/users", require("./routes/user.routes"));
 
 
 
